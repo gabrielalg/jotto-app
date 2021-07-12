972046ed161549671a73d8e5b0dd53dbe8b4c657
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2021-07-11 23:34:23 -0300

    First component

---
+++ src/components/Congrats.js
@@ -0,0 +1,20 @@
+/**
+ * Functional react component for congratulatory message.
+ * @function
+ * @param {object} props - React props.
+ * @returns {JSX.Element} - Rendered component (or null if `success` prop is false).
+ */
+
+export default function Congrats(props) {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats" className="alert alert-success">
+        <span data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </span>
+      </div>
+    );
+  } else {
+    return <div data-test="component-congrats" />;
+  }
+}
