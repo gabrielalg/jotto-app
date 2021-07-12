ff03a6715826b559fbef35d3adb94829ae024244
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2021-07-12 00:57:20 -0300

    Updating GuessedWords component

--- src/components/GuessedWords.js
+++ src/components/GuessedWords.js
@@ -4,6 +4,27 @@ function GuessedWords(props) {
     content = (
       <span data-test="guess-instructions">Try to guess the secret word!</span>
     );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word" key={index}>
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ));
+    content = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table className="table table-sm">
+          <thead className="thead-light">
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
   }
   return <div data-test="component-guessed-words">{content}</div>;
 }
