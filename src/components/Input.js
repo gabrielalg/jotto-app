a60af647b330c67517c645689355cc50c2689fc1
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2021-07-14 23:18:46 -0300

    Updating Input Tests

--- src/components/Input.js
+++ src/components/Input.js
@@ -16,8 +16,11 @@ function Input({ secretWord }) {
         <button
           data-test="submit-button"
           className="btn btn-primary mb-2"
-          onClick={(evt) => {
-            evt.preventDefault();
+          onClick={(event) => {
+            event.preventDefault();
+            // TODO: update guessedWords
+            // TODO: check against secretWord and update success if necessary
+            setCurrentGuess("");
           }}
         >
           Submit
--- src/components/Input.test.js
+++ src/components/Input.test.js
@@ -19,16 +19,31 @@ test("renders without error", () => {
 });
 
 describe("state controlled input field", () => {
-  test("state updates with value of input box upon change", () => {
-    const mockSetCurrentGuess = jest.fn();
-    React.useState = jest.fn(() => ["", mockSetCurrentGuess]);
+  let mockSetCurrentGuess = jest.fn();
+  let wrapper;
+  let originalUseState;
 
-    const wrapper = setup();
+  beforeEach(() => {
+    mockSetCurrentGuess.mockClear();
+    originalUseState = React.useState;
+    React.useState = () => ["", mockSetCurrentGuess];
+    wrapper = setup();
+  });
+  afterEach(() => {
+    React.useState = originalUseState;
+  });
+  test("state updates with value of input box upon change", () => {
     const inputBox = findByTestAttr(wrapper, "input-box");
 
     const mockEvent = { target: { value: "train" } };
     inputBox.simulate("change", mockEvent);
 
     expect(mockSetCurrentGuess).toHaveBeenCalledWith("train");
+  });
+  test("field is cleared upon submit button click", () => {
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+
+    submitButton.simulate("click", { preventDefault() {} });
+    expect(mockSetCurrentGuess).toHaveBeenCalledWith("");
   });
 });
