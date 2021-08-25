ce7ade611fb17232debca64695a7870682143c75
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2021-08-24 23:26:36 -0300

    Add functional tests for guess word

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import "./App.css";
 import Congrats from "./components/Congrats";
 import GuessedWords from "./components/GuessedWords";
+import Input from "./components/Input";
 
 function App() {
+  // TODO: get props from shared states
+  const success = false;
+  const secretWord = "party";
+  const guessedWords = [];
+
   return (
     <div data-test="component-app" className="container my-5">
       <h1>Jotto App</h1>
-      <Congrats success={true} />
-      <GuessedWords
-        guessedWords={[{ guessedWord: "train", letterMatchCount: 3 }]}
-      />
+      <Congrats success={success} />
+      <Input success={success} secretWord={secretWord} />
+      <GuessedWords guessedWords={guessedWords} />
     </div>
   );
 }
--- src/guessWord.test.js
+++ src/guessWord.test.js
@@ -6,7 +6,7 @@ import { findByTestAttr } from "../test/testUtils";
 /**
  * Create wrapper with specified initial conditions,
  * then submit a guessed word of 'train'
- # @function
+ ### @function
  *
  * @param {object} state - Initial conditions.
  * @returns {Wrapper} - Enzyme wrapper of mounted App component
@@ -26,8 +26,67 @@ const setup = (state = {}) => {
   return wrapper;
 };
 
-describe("no words guessed", () => {});
+describe.skip("no words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [],
+    });
+  });
+  test("creates GuessedWords table with one row", () => {
+    const guessedWordRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordRows).toHaveLength(1);
+  });
+});
 
-describe("some words guessed", () => {});
+describe.skip("some words guessed", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: "1" }],
+    });
+  });
+  test("creates GuessedWords table with two row", () => {
+    const guessedWordRows = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordRows).toHaveLength(2);
+  });
+});
 
-describe("guess secret word", () => {});
+describe.skip("guess secret word", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({
+      secretWord: "party",
+      success: false,
+      guessedWords: [{ guessedWord: "agile", letterMatchCount: "1" }],
+    });
+
+    // add value to input box
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    const mockEvent = { target: { value: "party" } };
+    inputBox.simulate("change", mockEvent);
+
+    // simulate click on submit button
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    submitButton.simulate("click", { preventDefault() {} });
+  });
+  test("adds row to guessedWords table", () => {
+    const guessedWordNodes = findByTestAttr(wrapper, "guessed-word");
+    expect(guessedWordNodes).toHaveLength(3);
+  });
+  test("displays congrats component", () => {
+    const congrats = findByTestAttr(wrapper, "component-congrats");
+    expect(congrats.text().length).toBeGreaterThan(0);
+  });
+  test("does not display input component contents", () => {
+    const inputBox = findByTestAttr(wrapper, "input-box");
+    expect(inputBox.exists()).toBe(false);
+
+    const submitButton = findByTestAttr(wrapper, "submit-button");
+    expect(submitButton.exists()).toBe(false);
+  });
+});
