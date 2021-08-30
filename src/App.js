e3dc74b2f72bb3b14d64714270cb2b5ead4d93aa
Gabriela Gonçalves <hi.gabidigital@gmail.com>
2021-08-29 22:27:51 -0300

    Implementing getSecretWord call

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useEffect } from "react";
 import "./App.css";
+
 import Congrats from "./components/Congrats";
 import GuessedWords from "./components/GuessedWords";
 import Input from "./components/Input";
+import { getSecretWord } from "./actions";
 
 function App() {
   // TODO: get props from shared states
   const success = false;
   const secretWord = "party";
   const guessedWords = [];
+
+  useEffect(() => {
+    getSecretWord();
+  }, []);
 
   return (
     <div data-test="component-app" className="container my-5">
